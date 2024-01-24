b85b7580b1887a3965ab68de1bdff8b46931ca1d
kodal <kodal@publicplan.de>
2024-01-24 21:45:21 +0100

    add `users` table and fixture data

--- migrations/initdb.sql
+++ migrations/initdb.sql
@@ -14,4 +14,19 @@ VALUES
     (1, 'Hello World', 'John Doe', 'Lorem Ipsum' , '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
     (2, 'Hello World 2', 'John Doe', 'Lorem Ipsum' , '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
     (3, 'Hello World 3', 'John Doe', 'Lorem Ipsum' , '2019-01-01 00:00:00', '2019-01-01 00:00:00');
-    
+
+CREATE table `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `password` TEXT NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `users` 
+    (`id`, `username`, `password`, `email`, `created_at`, `updated_at`)
+VALUES
+    (1, 'testuser1', '123', 'user@example.com' , '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    (2, 'testuser2', '123', 'user@example.com' , '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    (3, 'testuser3', '123', 'user@example.com' , '2019-01-01 00:00:00', '2019-01-01 00:00:00');
