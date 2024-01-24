aeaf33bb20ab0cf6dc6f852d74996d8fd9266462
kodal <kodal@publicplan.de>
2024-01-24 22:28:05 +0100

    introduce enriched display() twig proxy method
    controller's display() - method now serves as a proxy method
    for twig's display() method and adds
    * "user" session data
    * "messages"

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -3,9 +3,18 @@ namespace App\Controller;
 class AbstractController {
     protected $loader;
     protected $twig;
+    protected $messages = [];
     public function __construct() {
         $this->loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../../templates');
         $this->twig = new \Twig\Environment($this->loader);    
+    }
+    public function addMessage($message) {
+        $this->messages[] = $message;
+    }
+    public function display($template, $payload = []) {
+        $payload['user'] = $_SESSION['user'] ?? null;
+        $payload['messages'] = $this->messages;
+        $this->twig->display($template, $payload);
     }
     public function getDbConnection() {
         # database connection parameters
