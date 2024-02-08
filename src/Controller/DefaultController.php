56b69f28dac9ccb0d901839ccb75b345440da65b
kodal <kodal@publicplan.de>
2024-02-08 17:26:23 +0100

    optimize media upload (introduce thumbnail generator)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,11 +5,16 @@ namespace App\Controller;
 
 class DefaultController extends AbstractController
 {
-    
+    protected String $appRoot = '/var/www/html/public/';
+    protected String $uploadPath = '/var/www/html/public/assets/uploads/';
+    protected String $uploadPublicPath = '/assets/uploads/';
     public function index()
     {
         # retrieve results
         $results = $this->getDbConnection()->query("SELECT * FROM `posts`;")->fetchAll(\PDO::FETCH_ASSOC);
+        foreach ($results as &$result) {
+            $result['media'] = !empty($result['media']) ? json_decode($result['media']) : null;
+        }
         $this->setView('index.html', ['results' => $results]);
     }
 
@@ -105,35 +110,77 @@ class DefaultController extends AbstractController
             $result = $conn->query($sql); 
             $data['id'] = $conn->lastInsertId();
         }
-        $media = [];
-        $files = 0;
-        foreach($_FILES as $file) {
-            if ($file['error']) {
-                $this->addMessage(["code"=>422, "message"=>"File ".$file['name']." could not be uploaded."]);
-            }
-            if ($file['tmp_name'] && !$file['error']) {
-                $path = "assets/uploads/posts/$data[id]/";
-                $fullPath = "/var/www/html/public/$path";
-                if (!file_exists($fullPath)) {
-                    mkdir($fullPath, 0777, true);
-                }
-                $fileDestination = $fullPath .$file['name'];
-                if (!file_exists($fileDestination)) {
-                    move_uploaded_file($file['tmp_name'], $fileDestination);
-                }
-                $media[] = ['path'=>$path, 'name'=>$file['name'], 'size'=>filesize($fileDestination), 'original'=>$file['name']];           
-                $files++;
-            }
-        }
-        if ($files > 0) {
+        $media = $this->handleFileUploads("posts/$data[id]/");
+
+        # Store media array as JSON
+        if (count($media) > 0) {
             $sql = "UPDATE `posts` SET `media`='".json_encode($media)."' WHERE `id`='$data[id]'";
             $this->getDbConnection()->exec($sql);    
         }
         return $result;
     }
+    public function handleFileUploads($path):Array {
+        $media = [];
+        foreach($_FILES as $file) {
+            if ($file['error']) {
+                $this->addMessage(["code"=>422, "message"=>"File ".$file['name']." could not be uploaded. (error $file[error])"]);
+            }
+            if ($file['tmp_name'] && !$file['error']) {
+                $fullPath = $this->uploadPath . $path;
+
+                # Create Directory
+                if (!file_exists($fullPath)) {
+                    mkdir($fullPath, 0700, true);
+                }
+
+                # Generate unique filename
+                $file['target'] = $this->generateUniqueFilename($fullPath);
+                
+                # Generate and store thumbnail
+                $file['thumb'] = 't_' . $file['target'];
+                $this->generateImage($file['tmp_name'], $fullPath . $file['thumb'], 100, 100);
+                
+                # Move temporary file to final destination
+                $fileDestination = $fullPath . $file['target'];
+                if (!file_exists($fileDestination)) {
+                    move_uploaded_file($file['tmp_name'], $fileDestination);
+                }
+
+                # Create and append entry to $media array
+                $media[] = [
+                    'path'=>$path.$file['target'], 
+                    'type'=> $file['type'],
+                    'thumb'=>$path . $file['thumb'],
+                    'size'=>filesize($fileDestination), 
+                    'original'=>$file['name']
+                ];           
+            }
+        }
+        return $media;        
+    }
+    public function generateUniqueFilename($path, $prefix = '') {
+        $uniqueName = tempnam($path, $prefix);
+        unlink($uniqueName);
+        return substr($uniqueName, strlen($path));
+    }
+    public function generateImage(String $source, String $targetFilename, int $width, int $height, int $quality = 75) {
+        $targetImage = imagecreatetruecolor($width, $height);
+        $sourceImage = imagecreatefromjpeg($source);
+        list($w, $h) = getimagesize($source);
+        $ratio = $w / $h;
+        $newRatio = $width / $height;
+        imagecopyresampled($targetImage, $sourceImage, 0, 0, 0, 0, $width, $height, $w, $h);
+        return imagejpeg($targetImage, $targetFilename, $quality);
+    }
     public function postKill() {
         $id = $this->query['post_id'];
         $uid = $this->getUserId(true);
+        $media = $this->getDbConnection()->query("SELECT `media` FROM `posts` WHERE `id`='$id';")->fetchColumn();
+        $media = json_decode($media, true);
+        foreach($media as $file) {
+            unlink("/var/www/html/public/".$file['path'].$file['name']);
+            unlink("/var/www/html/public/".$file['thumb']);
+        }
         $result = $this->getDbConnection()->exec("DELETE FROM `posts` WHERE `id`='$id' AND `user`='$uid' LIMIT 1;");
         return $this->index();
     }
