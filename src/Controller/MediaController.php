bcbf8226b99b239483551c0a15dddb00dbdb109b
kodal <kodal@publicplan.de>
2024-02-08 17:57:56 +0100

    introduce dedicated MediaController

---
+++ src/Controller/MediaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+class MediaController extends AbstractController
+{
+    public function show() {
+        $postId = $this->query['post_id'];
+        $mediaId = $this->query['media_id'];
+        $result = $this->getDbConnection()->query("SELECT media FROM `posts` WHERE `id`='$postId';")->fetchColumn();
+        $media = json_decode($result, true);
+        $file = $media[$mediaId ? $mediaId : 0];
+        $filename = $this->uploadPath . $file['path'];
+        header("Content-Type: $file[type]");
+        header("Content-Length: ".filesize($filename));
+        echo file_get_contents($filename);
+        die();
+    }
+}
