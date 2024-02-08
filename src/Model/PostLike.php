f7f34d191c9fe8632fb3cbec285b2da80d0388e4
kodal <kodal@publicplan.de>
2024-02-08 23:54:09 +0100

    introduce model PostLike

---
+++ src/Model/PostLike.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Model;
+use App\Database;
+class PostLike extends AbstractModel {
+    protected static $table = "post_likes";
+    public static function insert($postId, $userId) {
+        return Database::connect()->exec("
+            INSERT INTO `post_likes` 
+            (`post`,`user`,`created_at`) VALUES 
+            ('$postId', '$userId', '".date('Y-m-d H:i:s')."');
+        ");            
+    }
+    public static function delete($postId, $userId) {
+        return Database::connect()->exec("
+            DELETE FROM `post_likes` WHERE `post`=$postId AND `user`=$userId;
+        ");
+
+    }
+}
