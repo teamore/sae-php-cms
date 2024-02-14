ffc611f4d49fe2afc0ea0283aaecd19ff4d5db6a
kodal <kodal@publicplan.de>
2024-02-14 22:02:32 +0100

    add static methods count(), getTable() and where()

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -4,8 +4,23 @@ use App\Database;
 class AbstractModel {
     protected static $table;
     public static function findBy($condition, $fields = '*') {
-        $table = static::$table;
-        return Database::connect()->query("SELECT $fields FROM `$table` WHERE $condition;")->fetch(\PDO::FETCH_ASSOC);
+        return Database::connect()->query("SELECT $fields FROM `".static::$table."` WHERE $condition;")->fetch(\PDO::FETCH_ASSOC);
+    }
+    public static function getTable(): string {
+        return static::$table;
+    }
+    public static function count(?array $criteria = null): ?int {
+        $sql = "SELECT COUNT(*) FROM `".static::$table."` ".static::where($criteria);
+        return Database::connect()->query($sql.";")->fetchColumn();
+    }
+    public static function where(?array $criteria = null):string {
+        $sql = " WHERE 1 ";
+        if (isset($criteria)) {
+            foreach($criteria as $field => $value) {
+                $sql .= " AND `$field`='$value'";
+            }
+        }
+        return $sql;
     }
     public function db() {
         $pdo = Database::connect();
