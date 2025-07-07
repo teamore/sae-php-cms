c0759d723ec136fb80250225b0f0bfb76257d6ce
kodal <kodal@publicplan.de>
2025-07-07 20:41:40 +0200

    require exact case matches when creating qualified class name

--- public/index.php
+++ public/index.php
@@ -6,7 +6,10 @@ require '../vendor/autoload.php';
 
 spl_autoload_register(function ($class) {
     $class = str_replace("\\",DIRECTORY_SEPARATOR,$class);
-    $search = [str_replace("App","/../src",$class), str_replace("Twig","/../vendor/twig",$class)];
+    $search = [
+        preg_replace("/^App/","/../src",$class), 
+        preg_replace("/^Twig/","/../vendor/twig",$class)
+    ];
     foreach($search as $qcn) {
         if (file_exists(__DIR__ . $qcn . '.php')) {
             require_once(__DIR__ . $qcn . '.php');
